d8f0168f720364d75052956ee07fe7d47925c81b
darshvaghela <darshv@carstream.co>
2023-05-05 16:18:27 +0530

    pagination with server side

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from "react";
 import MyTable from "./MyTable";
-import { gql, useQuery } from "urql";
 
-const getSong = gql`
-  query {
-    song {
-      id
-      name
-      artist
-      moviename
-    }
-  }
-`;
+
 
 function App() {
-  const [result, reexecuteQuery] = useQuery({
-    query: getSong,
-  });
-  const { data, fetching, error } = result;
+ 
   return (
     <>
-      <MyTable data={data} />
+      <MyTable />
     </>
   );
 }
--- src/MyTable.tsx
+++ src/MyTable.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { gql, useQuery } from "urql";
 import type { ColumnsType } from "antd/es/table";
 import Table from "antd/es/table";
+import { Pagination } from "antd";
 
 interface DataType {
   key: number;
@@ -9,7 +10,6 @@ interface DataType {
   artist: string;
   movieName: string;
 }
-
 const columns: ColumnsType<DataType> = [
   {
     title: "Name",
@@ -30,9 +30,41 @@ const columns: ColumnsType<DataType> = [
 
 const MyTable = (props: any) => {
   const [tableData, setTableData] = useState<DataType[]>([]);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  const offset = (page - 1) * 5;
 
+  const getSong = gql`
+  query {
+    song(limit: ${pageSize.toString()}, offset: ${offset.toString()} ) {
+      id
+      name
+      artist
+      moviename
+    }
+    song_aggregate {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
+
+  const [songResult, reexecuteQuery] = useQuery({
+    query: getSong,
+  });
+
+  const songData = songResult?.data;
+  const totalSongs = songResult?.data?.song_aggregate?.aggregate.count;
+
+  const handleOnPageChange = (newPage: any) => {
+    setPage(newPage);
+  };
   useEffect(() => {
-    props?.data?.song.map((item: any) => {
+    if (tableData.length > 0) {
+      setTableData([]);
+    }
+    songData?.song.map((item: any) => {
       setTableData((prev) => [
         ...prev,
         {
@@ -43,13 +75,26 @@ const MyTable = (props: any) => {
         },
       ]);
     });
-  }, [props]);
-
+  }, [songData, page]);
   return (
     <>
       <div>
         {tableData?.length > 0 && (
-          <Table columns={columns} dataSource={tableData} />
+          <>
+            <Table
+              columns={columns}
+              dataSource={tableData}
+              pagination={false}
+            />
+            <div style={{ float: "right", marginTop: "20px" }}>
+              <Pagination
+                current={page}
+                pageSize={pageSize}
+                total={totalSongs}
+                onChange={handleOnPageChange}
+              />
+            </div>
+          </>
         )}
       </div>
     </>
